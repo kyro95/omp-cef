cd813c7775efa77af9694618b473b246ce97fc8d
NeekoGta <gtaneeko@gmail.com>
2026-02-03 11:50:39 +0100

    chore(client): normalize CefApp switches

--- src/client/core/browser/app.hpp
+++ src/client/core/browser/app.hpp
@@ -9,21 +9,10 @@ public:
     void OnBeforeCommandLineProcessing(const CefString& process_type, CefRefPtr<CefCommandLine> command_line) override 
     {
         command_line->AppendSwitchWithValue("force-device-scale-factor", "1.0");
-        command_line->AppendSwitch("high-dpi-support");
-
-        command_line->AppendSwitch("use-gl=swiftshader-webgl");
-        command_line->AppendSwitch("enable-unsafe-swiftshader");
-        command_line->AppendSwitch("disable-gpu-vsync");
-        
-        command_line->AppendSwitch("ignore-gpu-blocklist");
-        
-        command_line->AppendSwitch("enable-font-antialiasing");
+        command_line->AppendSwitchWithValue("high-dpi-support", "1");
+        command_line->AppendSwitch("enable-begin-frame-scheduling");
         command_line->AppendSwitchWithValue("autoplay-policy", "no-user-gesture-required");
-
-        command_line->AppendSwitch("disable-web-security");
-        command_line->AppendSwitch("disable-site-isolation-trials");
-        
-        command_line->AppendSwitch("disable-features=IsolateOrigins,site-per-process,CrossOriginOpenerPolicy");
+        command_line->AppendSwitchWithValue("allow-browser-signin", "false");
     }
 
 private:
